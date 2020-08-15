15c7c7cf8475b730fbbffd427c4d0312a95e64c7
Thais Ayumi <thaisayumi@MacBook-Air-de-Thais.local>
2020-08-15 01:13:31 -0300

    Primeiro commit

---
+++ aula 1/hello.py
@@ -0,0 +1,14 @@
+def lista():
+    nome = input("digite o seu nome:")
+    idade = input("digite sua idade:")
+    resultado = nome + " tem " + idade + " anos "
+    print(resultado)
+
+def par(rangeNumbers):
+    for number in range(rangeNumbers):
+        if number % 2 == 0:
+            print(number)
+
+
+lista() 
+par(100)           
